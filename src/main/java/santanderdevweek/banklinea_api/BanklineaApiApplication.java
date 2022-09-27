91f9d3fe9842b62dc49eca6b1cc0dca63784a136
carlos738 <carlosasl135@hotmail.com>
2022-09-27 20:47:47 -0300

    banklina-api

---
+++ src/main/java/santanderdevweek/banklinea_api/BanklineaApiApplication.java
@@ -0,0 +1,13 @@
+package santanderdevweek.banklinea_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/santanderdevweek/banklinea_api/BanklineaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package santanderdevweek.banklinea_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
